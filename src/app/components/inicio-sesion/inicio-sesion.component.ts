cdf162a1b99c984bc35c61512001bb56ebfc91da
Luisa Giraldo <luiisagiraldori@gmail.com>
2024-08-04 17:06:36 -0500

    Add form

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../encabezado/encabezado.component';
 import { PieDePaginaComponent } from '../pie-de-pagina/pie-de-pagina.component';
+import { BtnprimaryDirective } from '../../directives/btnprimary.directive';
+import { InputFormCustomDirective } from '../../directives/input-form-custom.directive';
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [EncabezadoComponent, PieDePaginaComponent],
+  imports: [
+    EncabezadoComponent,
+    PieDePaginaComponent,
+    BtnprimaryDirective,
+    InputFormCustomDirective,
+  ],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.scss',
 })
