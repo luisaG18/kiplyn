e420df098084357ca07a28e6b99c08ed461c0bc7
Luisa Giraldo <luiisagiraldori@gmail.com>
2024-08-05 15:42:41 -0500

    Add styles to register component

--- src/app/components/registrarse/registrarse.component.ts
+++ src/app/components/registrarse/registrarse.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../general-components/encabezado/encabezado.component';
 import { PieDePaginaComponent } from '../general-components/pie-de-pagina/pie-de-pagina.component';
+import { InputFormCustomDirective } from '../../directives/input-form-custom.directive';
+import { BtnprimaryDirective } from '../../directives/btnprimary.directive';
 
 @Component({
   selector: 'app-registrarse',
   standalone: true,
-  imports: [EncabezadoComponent, PieDePaginaComponent],
+  imports: [EncabezadoComponent, PieDePaginaComponent, InputFormCustomDirective, BtnprimaryDirective],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.scss',
 })
