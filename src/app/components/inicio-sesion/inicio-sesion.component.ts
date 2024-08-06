1e71d993e811f2c23c82f1b09d7eb51dea056d8d
Luisa Giraldo <luiisagiraldori@gmail.com>
2024-08-05 23:39:58 -0500

    Add ngModel in form login

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -5,23 +5,37 @@ import { BtnprimaryDirective } from '../../directives/btnprimary.directive';
 import { InputFormCustomDirective } from '../../directives/input-form-custom.directive';
 import { BtnsecondaryDirective } from '../../directives/btnsecondary.directive';
 import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { usuarioLogin } from '../../models/usuarioLogin';
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [EncabezadoComponent, PieDePaginaComponent, BtnprimaryDirective, InputFormCustomDirective, BtnsecondaryDirective],
+  imports: [EncabezadoComponent, PieDePaginaComponent, BtnprimaryDirective, InputFormCustomDirective, BtnsecondaryDirective, FormsModule],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.scss',
 })
 export class InicioSesionComponent {
-  // Variable del color del botón secundary
-  colorText = 'Black';
+  loginInfo: usuarioLogin = {
+    usuario: '',
+    password: '',
+  };
 
   constructor(private router: Router) {}
+
   /**
    * Función para ir a la pagina de registrar
    */
   goToRegister() {
     this.router.navigate(['/register']);
+  }
+
+  /**
+   * Función enviar formulario
+   */
+  submitForm(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    }
   }
 }
---
+++ src/app/models/usuarioLogin.ts
@@ -0,0 +1,4 @@
+export interface usuarioLogin {
+  usuario: '';
+  password: '';
+}
